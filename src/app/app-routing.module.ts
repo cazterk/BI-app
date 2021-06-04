2fa1c2bb1763cbd051891062b51362c6b0fa900e
Cephas Zulu <48860145+cazterk@users.noreply.github.com>
2021-06-04 12:41:37 +0200

    login working as root path

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,7 +6,7 @@ import { Routes, RouterModule } from '@angular/router';
 
 
 const routes: Routes = [
-  {path:'login', component: LoginComponent},{path:'dashboard', component:DashboardComponent}
+  {path:'', component: LoginComponent},{path:'dashboard', component:DashboardComponent}
 ];
 
 @NgModule({
